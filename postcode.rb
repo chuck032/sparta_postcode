31fd905b1c9847e3ca9320dc68774b6771b1b231
Tech-A38 <tech-a38@Tech-A38s-MacBook-Air.local>
2018-06-12 00:37:22 +0100

    completed half multi test

--- postcode.rb
+++ postcode.rb
@@ -34,11 +34,146 @@ class Postcodesio
     get_result(postcode)["eastings"]
   end
 
+  def get_country(postcode)
+    get_result(postcode)["country"]
+  end
+
+  def get_NHS(postcode)
+    get_result(postcode)["nhs_ha"]
+  end
+
+  def get_longitude(postcode)
+    get_result(postcode)["longitude"]
+  end
+
+  def get_latitude(postcode)
+    get_result(postcode)["latitude"]
+  end
+
+  def get_parliamentary(postcode)
+    get_result(postcode)["parliamentary_constituency"]
+  end
+
+  def get_european(postcode)
+    get_result(postcode)["european_electoral_region"]
+  end
+
+  def get_primary_care(postcode)
+    get_result(postcode)["primary_care_trust"]
+  end
+
+  def get_region(postcode)
+    get_result(postcode)["region"]
+  end
+
+  def get_parish(postcode)
+    get_result(postcode)["parish"]
+  end
+
+  def get_lsoa(postcode)
+    get_result(postcode)["lsoa"]
+  end
+
+  def get_msoa(postcode)
+    get_result(postcode)["msoa"]
+  end
+
+  def get_admin_district(postcode)
+    get_result(postcode)["admin_district"]
+  end
+
+  def get_incode(postcode)
+    get_result(postcode)["incode"]
+  end
+
+  def get_incode_length(postcode)
+    get_incode(postcode).length
+  end
+
+  def get_query_index(postcode, index)
+    get_result(postcode)[index]["query"]
+  end
+
+  def get_results_hash(postcode)
+      result_hash_array = []
+      get_result(postcode).each  do |x|
+          result_hash_array << x["result"].class
+      end
+      result_hash_array
+  end
+
+  def get_postcodes_length(postcode)
+    postcode_array = []
+    get_result(postcode).each  do |x|
+        postcode_array << x["result"]["postcode"].length.to_i
+    end
+    postcode_array
+  end
+
+  def get_quality_key(postcode)
+    quality_array = []
+    get_result(postcode).each  do |x|
+        quality_array << x["result"]["quality"].to_i
+    end
+    quality_array
+  end
+
+  def get_eastings_value(postcode)
+    eastings_array = []
+    get_result(postcode).each  do |x|
+      eastings_array << x["result"]["eastings"].to_i
+    end
+    eastings_array
+  end
+
+  def get_countries(postcode)
+    country_array = []
+    get_result(postcode).each  do |x|
+      country_array << x["result"]["country"]
+    end
+    country_array
+  end
+
+  def get_NHS_authority(postcode)
+    nhs_array = []
+    get_result(postcode).each  do |x|
+      nhs_array << x["result"]["nhs_ha"]
+    end
+    nhs_array
+  end
+
+  def get_long(postcode)
+    long_array = []
+    get_result(postcode).each  do |x|
+      long_array << x["result"]["longitude"]
+    end
+    long_array
+  end
+
+  def get_lat(postcode)
+    lat_array = []
+    get_result(postcode).each  do |x|
+      lat_array << x["result"]["latitude"]
+    end
+    lat_array
+  end
+
+  def get_parl_const(postcode)
+    parliamentary_array = []
+    get_result(postcode).each  do |x|
+      parliamentary_array << x["result"]["parliamentary_constituency"]
+    end
+    parliamentary_array
+  end
+
 
 
 
 end
 
-# new_postcodes = Postcodesio.new
-# p new_postcodes.get_single_postcode("HA80PX")["status"]
-# p new_postcodes.get_status
+new_postcodes = Postcodesio.new
+array = ["HA80PX","CH53PA","EH24ET","BT11AA"]
+# p new_postcodes.get_single_postcode("HA80PX")
+# p new_postcodes.get_incode(new_postcodes.get_single_postcode("HA80PX"))
+# p new_postcodes.get_multiple_postcodes(array)
+p new_postcodes.get_parl_const(new_postcodes.get_multiple_postcodes(array))
--- spec/postcode_spec.rb
+++ spec/postcode_spec.rb
@@ -6,7 +6,7 @@ describe Postcodesio do
 
     before(:all) do
       @postcodesio = Postcodesio.new
-      @response = @postcodesio.get_single_postcode('HA80PX') #input a postcode
+      @response = @postcodesio.get_single_postcode('ha80px') #input a postcode
     end
 
     it "should respond with a status message of 200" do
@@ -30,51 +30,60 @@ describe Postcodesio do
     end
 
     it "should return a country which is one of the four constituent countries of the UK" do
-      # p @response["result"]["country"]
-      # expect(@response["result"]["country"]).to eq("England" || "Wales" || "Scotland" || "")
+      expect(@postcodesio.get_country(@response)).to eq("England").or eq("Wales").or eq("Scotland").or eq("Northern Ireland")
     end
 
     it "should return a string value for NHS authority " do
+      expect(@postcodesio.get_NHS(@response)).to be_kind_of String
     end
 
     it "should return a longitude float value" do
+      expect(@postcodesio.get_longitude(@response)).to be_kind_of Float
     end
 
     it "should return a latitude float value" do
+      expect(@postcodesio.get_latitude(@response)).to be_kind_of Float
     end
 
     it "should return a parliamentary constituency string" do
+      expect(@postcodesio.get_parliamentary(@response)).to be_kind_of String
     end
 
     it "should return a european_electoral_region string" do
+      expect(@postcodesio.get_european(@response)).to be_kind_of String
     end
 
     it "should return a primary_care_trust string" do
+      expect(@postcodesio.get_primary_care(@response)).to be_kind_of String
     end
 
     it "should return a region string" do
+      expect(@postcodesio.get_region(@response)).to be_kind_of String
     end
 
     it "should return a parish string" do
+      expect(@postcodesio.get_parish(@response)).to be_kind_of String
     end
 
     it "should return a lsoa string" do
+      expect(@postcodesio.get_lsoa(@response)).to be_kind_of String
     end
 
     it "should return a msoa string" do
+      expect(@postcodesio.get_msoa(@response)).to be_kind_of String
     end
     # admin ward and county are not documented however tested below
 
     it "should return a admin_district string" do
+      expect(@postcodesio.get_admin_district(@response)).to be_kind_of String
     end
 
     it "should return a incode string of three characters" do
-    end
-
-    it "should return a msoa string" do
+      expect(@postcodesio.get_incode(@response)).to be_kind_of String
     end
 
     it "should return a incode string of 3-4 characters" do
+      expect(@postcodesio.get_incode_length(@response)).to be_between(3 , 4)
     end
   end
 
@@ -82,43 +91,69 @@ describe Postcodesio do
 
     before(:all) do
       @postcodesio = Postcodesio.new
-      @response = @postcodesio.get_multiple_postcodes(["HA80PX, M32 0JG"]) #Add in array of postcodes
+      @response = @postcodesio.get_multiple_postcodes(["HA80PX","CH53PA","EH24ET","BT11AA"]) #Add in array of postcodes
     end
 
     it "should respond with a status message of 200" do
+      expect(@postcodesio.get_status(@response)).to eq 200
     end
 
     it "should return the first query as the first postcode in the response" do
+      # expect(@postcodesio.get_first_query(@response)).to include("HA80PX").or include("CH53PA").or include("EH24ET").or include("BT11AA")
+      expect(@postcodesio.get_query_index(@response, 0)).to eq("HA80PX")
     end
 
-    it "should return the second query as the first postcode in the response" do
+    it "should return the second query as the second postcode in the response" do
+      expect(@postcodesio.get_query_index(@response, 1)).to eq("CH53PA")
     end
 
     it "should have a results hash" do
+      @postcodesio.get_results_hash(@response).each do |x|
+        expect(x).to eq(Hash)
+      end
     end
 
     it "should return a postcode between 5-7 in length"  do
+      @postcodesio.get_postcodes_length(@response).each do |x|
+        expect(x).to be_between(5,7)
+      end
     end
 
     it "should return an quality key integer between 1-9" do
+      @postcodesio.get_quality_key(@response).each do |x|
+        expect(x).to be_kind_of Integer
+        expect(x).to be_between(1,9)
+      end
     end
 
     it "should return an ordnance survey eastings value as integer" do
-    end
-
-    it "should return an ordnance survey eastings value as integer" do
+      @postcodesio.get_eastings_value(@response).each do |x|
+        expect(x).to be_kind_of Integer
+      end
     end
 
     it "should return a country which is one of the four constituent countries of the UK" do
+      @postcodesio.get_countries(@response).each do |x|
+        expect(x).to eq("England").or eq("Wales").or eq("Scotland").or eq("Northern Ireland")
+      end
     end
 
     it "should return a string value for NHS authority " do
+      @postcodesio.get_NHS_authority(@response).each do |x|
+        expect(x).to be_kind_of String
+      end
     end
 
     it "should return a longitude float value" do
+      @postcodesio.get_long(@response).each do |x|
+        expect(x).to be_kind_of Float
+      end
     end
 
     it "should return a latitude float value" do
+      @postcodesio.get_lat(@response).each do |x|
+        expect(x).to be_kind_of Float
+      end
     end
 
     it "should return a parliamentary constituency string" do
@@ -147,70 +182,6 @@ describe Postcodesio do
     end
 
     it "should return a incode string of three characters" do
-    end
-
-    it "should return a msoa string" do
-    end
-
-    it "should return a incode string of 3-4 characters" do
-    end
-
-    it "should have a results hash" do
-    end
-
-    it "should return a postcode between 5-7 in length"  do
-    end
-
-    it "should return an quality key integer between 1-9" do
-    end
-
-    it "should return an ordnance survey eastings value as integer" do
-    end
-
-    it "should return an ordnance survey eastings value as integer" do
-    end
-
-    it "should return a country which is one of the four constituent countries of the UK" do
-    end
-
-    it "should return a string value for NHS authority " do
-    end
-
-    it "should return a longitude float value" do
-    end
-
-    it "should return a latitude float value" do
-    end
-
-    it "should return a parliamentary constituency string" do
-    end
-
-    it "should return a european_electoral_region string" do
-    end
-
-    it "should return a primary_care_trust string" do
-    end
-
-    it "should return a region string" do
-    end
-
-    it "should return a parish string" do
-    end
-
-    it "should return a lsoa string" do
-    end
-
-    it "should return a msoa string" do
-    end
-    # admin ward and county are not documented however tested below
-
-    it "should return a admin_district string" do
-    end
-
-    it "should return a incode string of three characters" do
-    end
-
-    it "should return a msoa string" do
     end
 
     it "should return a incode string of 3-4 characters" do
