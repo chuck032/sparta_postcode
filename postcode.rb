dc8501c778f4e4d47d175f4b9572a65627e0688c
Tech-A38 <tech-a38@Tech-A38s-MacBook-Air.local>
2018-06-12 01:35:33 +0100

    changed comments

--- postcode.rb
+++ postcode.rb
@@ -20,103 +20,103 @@ class Postcodesio
     postcode["status"]
   end
 
-# Method to find result key
+# Method to find result
   def get_result(postcode)
     postcode["result"]
   end
 
-# Method to find length of postcode key within result hash
+# Method to find length of postcode within result hash
   def get_postcode_length(postcode)
     get_result(postcode)["postcode"].length
   end
 
-  # Method to find quality key within result hash
+  # Method to find quality within result hash
   def get_quality(postcode)
     get_result(postcode)["quality"]
   end
 
-  # Method to find eastings key within result hash
+  # Method to find eastings within result hash
   def get_eastings(postcode)
     get_result(postcode)["eastings"]
   end
 
-  # Method to find country key within result hash
+  # Method to find country within result hash
   def get_country(postcode)
     get_result(postcode)["country"]
   end
 
-  # Method to find NHS authority key within result hash
+  # Method to find NHS authority within result hash
   def get_NHS(postcode)
     get_result(postcode)["nhs_ha"]
   end
 
-  # Method to find longitude key within result hash
+  # Method to find longitude within result hash
   def get_longitude(postcode)
     get_result(postcode)["longitude"]
   end
 
-  # Method to find latitude key within result hash
+  # Method to find latitude within result hash
 def get_latitude(postcode)
     get_result(postcode)["latitude"]
   end
 
-  # Method to find parliamentary constituency key within result hash
+  # Method to find parliamentary constituency within result hash
   def get_parliamentary(postcode)
     get_result(postcode)["parliamentary_constituency"]
   end
 
-  # Method to find European electoral region key within result hash
+  # Method to find European electoral region within result hash
   def get_european(postcode)
     get_result(postcode)["european_electoral_region"]
   end
 
-  # Method to find primary care trust key within result hash
+  # Method to find primary care trust within result hash
   def get_primary_care(postcode)
     get_result(postcode)["primary_care_trust"]
   end
 
-  # Method to find region key within result hash
+  # Method to find region within result hash
   def get_region(postcode)
     get_result(postcode)["region"]
   end
 
-  # Method to find parish key within result hash
+  # Method to find parish within result hash
   def get_parish(postcode)
     get_result(postcode)["parish"]
   end
 
-  # Method to find lsoa key within result hash
+  # Method to find lsoa within result hash
   def get_lsoa(postcode)
     get_result(postcode)["lsoa"]
   end
 
-  # Method to find msoa key within result hash
+  # Method to find msoa within result hash
   def get_msoa(postcode)
     get_result(postcode)["msoa"]
   end
 
-  # Method to find admin district key within result hash
+  # Method to find admin district within result hash
   def get_admin_district(postcode)
     get_result(postcode)["admin_district"]
   end
 
-  # Method to find incode key within result hash
+  # Method to find incode within result hash
   def get_incode(postcode)
     get_result(postcode)["incode"]
   end
 
-  # Method to find the length of the incode key
+  # Method to find the length of the incode
   def get_incode_length(postcode)
     get_incode(postcode).length
   end
 
   # Method multi-postcodes
-  # Method to find query key within the index of the array and result hash
+  # Method to find query within the index of the array and result hash
   def get_query_index(postcode, index)
     get_result(postcode)[index]["query"]
   end
 
-  # Method to find data type of all postcodes result result key
+  # Method to find data type of all postcodes
   def get_results_hash(postcode)
       result_hash_array = []
       get_result(postcode).each  do |x|
@@ -134,7 +134,7 @@ def get_latitude(postcode)
     postcode_array
   end
 
-  # Method to find the quality keys within the result hash, for all postcodes
+  # Method to find quality within the result hash, for all postcodes
   def get_quality_key(postcode)
     quality_array = []
     get_result(postcode).each  do |x|
@@ -143,7 +143,7 @@ def get_latitude(postcode)
     quality_array
   end
 
-  # Method to find the eastings keys within the result hash, for all postcodes
+  # Method to find eastings within the result hash, for all postcodes
     def get_eastings_value(postcode)
     eastings_array = []
     get_result(postcode).each  do |x|
@@ -152,7 +152,7 @@ def get_latitude(postcode)
     eastings_array
   end
 
-  # Method to find the country keys within the result hash, for all postcodes
+  # Method to find country within the result hash, for all postcodes
   def get_countries(postcode)
     country_array = []
     get_result(postcode).each  do |x|
@@ -161,7 +161,7 @@ def get_latitude(postcode)
     country_array
   end
 
-  # Method to find the NHS authority keys within the result hash, for all postcodes
+  # Method to find NHS authority within the result hash, for all postcodes
   def get_NHS_authority(postcode)
     nhs_array = []
     get_result(postcode).each  do |x|
@@ -170,7 +170,7 @@ def get_latitude(postcode)
     nhs_array
   end
 
-  # Method to find the longitude keys within the result hash, for all postcodes
+  # Method to find longitude within the result hash, for all postcodes
   def get_long(postcode)
     long_array = []
     get_result(postcode).each  do |x|
@@ -179,7 +179,7 @@ def get_latitude(postcode)
     long_array
   end
 
-  # Method to find the latitude keys within the result hash, for all postcodes
+  # Method to find latitude within the result hash, for all postcodes
   def get_lat(postcode)
     lat_array = []
     get_result(postcode).each  do |x|
@@ -188,7 +188,7 @@ def get_latitude(postcode)
     lat_array
   end
 
-  # Method to find the parliamentary constituency keys within the result hash, for all postcodes
+  # Method to find parliamentary constituency within the result hash, for all postcodes
   def get_parl_const(postcode)
     parliamentary_array = []
     get_result(postcode).each  do |x|
@@ -197,7 +197,7 @@ def get_latitude(postcode)
     parliamentary_array
   end
 
-  # Method to find the European electoral region keys within the result hash, for all postcodes
+  # Method to find European electoral region within the result hash, for all postcodes
   def get_euro_elect(postcode)
     euro_array = []
     get_result(postcode).each  do |x|
@@ -206,7 +206,7 @@ def get_latitude(postcode)
     euro_array
   end
 
-  # Method to find the primary care trust keys within the result hash, for all postcodes
+  # Method to find primary care trust within the result hash, for all postcodes
   def get_prim_care(postcode)
     primary_array = []
     get_result(postcode).each  do |x|
@@ -215,7 +215,7 @@ def get_latitude(postcode)
     primary_array
   end
 
-  # Method to find the region keys within the result hash, for all postcodes, excluding nil values
+  # Method to find region within the result hash, for all postcodes, excluding nil values
   def get_reg(postcode)
     region_array = []
     get_result(postcode).each  do |x|
@@ -226,7 +226,7 @@ def get_latitude(postcode)
     region_array
   end
 
-  # Method to find the parish keys within the result hash, for all postcodes, excluding nil values
+  # Method to find parish within the result hash, for all postcodes, excluding nil values
   def get_par(postcode)
     parish_array = []
     get_result(postcode).each  do |x|
@@ -237,7 +237,7 @@ def get_latitude(postcode)
     parish_array
   end
 
-  # Method to find the lsoa keys within the result hash, for all postcodes
+  # Method to find lsoa within the result hash, for all postcodes
   def get_lsoa_mult(postcode)
     lsoa_array = []
     get_result(postcode).each  do |x|
@@ -246,7 +246,7 @@ def get_latitude(postcode)
     lsoa_array
   end
 
-  # Method to find the msoa keys within the result hash, for all postcodes, excluding nil values
+  # Method to find msoa within the result hash, for all postcodes, excluding nil values
   def get_msoa_mult(postcode)
     msoa_array = []
     get_result(postcode).each  do |x|
@@ -257,7 +257,7 @@ def get_latitude(postcode)
     msoa_array
   end
 
-  # Method to find the admin district keys within the result hash, for all postcodes
+  # Method to find admin district within the result hash, for all postcodes
   def get_admin_dis(postcode)
     admin_array = []
     get_result(postcode).each  do |x|
@@ -266,7 +266,7 @@ def get_latitude(postcode)
     admin_array
   end
 
-  # Method to find the length of incode keys within the result hash, for all postcodes
+  # Method to find length of incode within the result hash, for all postcodes
   def get_incode_char(postcode)
     incode_array = []
     get_result(postcode).each  do |x|
